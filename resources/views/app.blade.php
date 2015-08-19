57b332af0a6363d0c883d75ca91dc685999ace71
Marcus Reinhardt <webstone@gmail.com>
2015-08-19 13:53:55 +0200

    meta update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,11 +2,11 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>{{ isset($title) ? $title . ' - ' : null }}Laravel - The PHP Framework For Web Artisans</title>
+	<title>Mit Liebe und Leidenschaft gestaltet. › pseudoagentur</title>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="author" content="Taylor Otwell">
-	<meta name="description" content="Laravel - The PHP framework for web artisans.">
-	<meta name="keywords" content="laravel, php, framework, web, artisans, taylor otwell">
+	<meta name="author" content="Pseudoagentur">
+	<meta name="description" content="Mit Liebe und Leidenschaft gestaltet.">
+	<meta name="keywords" content="laravel, cartalyst, sentinel, sleepingowl, admin, documentation, pseudoagentur, anders, aber, richtig">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!--[if lte IE 9]>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
